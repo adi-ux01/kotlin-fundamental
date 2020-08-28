1f624bb9436e8a2ae581613f4f90681144b64a72
adi-ux01 <70367501+adi-ux01@users.noreply.github.com>
2020-08-28 20:32:55 +0700

    Add files via upload

---
+++ 1. Hello Kotlin.kt
@@ -0,0 +1,11 @@
+fun main () {
+    val name = "Adi"
+    val hobi = "Naik Motor"
+
+    print("Halo nama saya   :")
+    println(name)
+    print("Hobi saya adalah :")
+    println(hobi)
+    print(if (true)"always true" else "always false")
+
+}
